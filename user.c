b3906aa9d9655b566e1971c5b7b4900f6fda36b3
pwu66 <pwu66@naver.com>
2019-04-10 22:33:23 +0900

    list and others

--- user.c
+++ user.c
@@ -3,7 +3,22 @@
 //user.tct의 파일에 있는 사용자 id,password 읽어오기
 int load_file(LOGIN* list[], char* filename){
   int count=0;
-  FILE *datafile = fopen(filename, "r");
+  int choice;
+ FILE *datafile = fopen(filename, "r");
+
+if(datafile == NULL){
+   printf("Error!!\n");
+   printf("%s file not exist! makew anyway?(yes1, no 2) >> \n",filename);
+   scanf("%d",&choice);
+
+   if(choice ==1){
+      printf("Welcome\n");
+      datafile = fopen(filename,"w");
+ 	fclose(datafile);
+    }	return 0;
+  }
+
+   datafile = fopen(filename,"r");
   while(!feof(datafile)){
     list[count]=(LOGIN*)malloc(sizeof(LOGIN));
     fscanf(datafile,"%s %s",list[count]->id,list[count]->password);
@@ -11,6 +26,7 @@ int load_file(LOGIN* list[], char* filename){
   }
   printf("%d records read!\n",count);\
   fclose(datafile);
+  count--;
   return count;
 }
 
@@ -19,11 +35,11 @@ void join(LOGIN* list[], int count){
   char id[20], pass[20];
   while(1){
     printf("Enter new user id >> ");
-    scanf("%s", id);
-    int dup=0;
+    scanf("%s", id); 
+    int dup=0;//현재 아이디 없는 경우 0
     for(int i=0;i<count;i++){//현재 있는 id 인지 check
       if(strcmp(id, list[i]->id)==0) {
-        dup=1; break;
+        dup=1; break; //있으면 dup = 1
       }
     }
     if(dup==1){
@@ -33,7 +49,7 @@ void join(LOGIN* list[], int count){
       printf("Enter password >> ");
       scanf("%s", pass);
       list[count] = (LOGIN*)malloc(sizeof(LOGIN));
-      strcpy(list[count]->id, id);
+      strcpy(list[count]->id, id); //비밀번호 입력 후 , 아이디와 비번을 포인터에 저장한다.
       strcpy(list[count]->password, pass);
       printf("New user added!\n");
       break;
@@ -45,15 +61,15 @@ int login(LOGIN* list[], int count){
   char id[20], pass[20];
   printf("Enter user id >> ");
   scanf("%s", id);
-  int dup=0, found;
+  int dup=0, found; //아이디가 기존에 있는 아이디인지 찾기
   for(int i=0;i<count;i++){
     if(strcmp(id, list[i]->id)==0) {
-      dup=1;
+      dup=1; //기존 회원이면 dup =1
       found = i;
       break;
     }
   }
-  if(dup!=1){
+  if(dup!=1){ 
     printf("No such user!!\n");
     return -1;
   }
@@ -83,4 +99,10 @@ void save_file(LOGIN* list[], int count, char* filename){
   }
   printf("%s Saved!\n", filename);
   fclose(datafile);
+}
+
+void list(LOGIN* list[], int count){
+    for(int i=0; i<count; i++){
+     printf("[%d]%s / %s\n",i+1,list[i]->id, list[i]->password); 
+ }
 }
